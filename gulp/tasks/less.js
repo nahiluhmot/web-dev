9c5ec6313c00c8fe43ed29a90731aadf4c7329ed
Tom Hulihan <hulihan.tom159@gmail.com>
2015-02-04 12:03:47 -0500

    Gracefully handle errors in gulp.
    This prevents the watch task from aborting because of a syntax error.

--- gulp/tasks/less.js
+++ gulp/tasks/less.js
@@ -1,5 +1,6 @@
 var config = require('../config.js').less,
     gulp = require('gulp'),
+    handleErrors = require('../util/handleErrors.js');
     less = require('gulp-less');
 
 /**
@@ -8,5 +9,6 @@ var config = require('../config.js').less,
 gulp.task('less', function() {
   return gulp.src(config.src)
     .pipe(less({ paths: config.paths }))
+    .on('error', handleErrors)
     .pipe(gulp.dest(config.dest));
 });
